18c50207be76537fdfb50064edb2d8ef8142f36f
eurtiom <62328787+eurtiom@users.noreply.github.com>
2022-06-30 16:50:34 +0200

    Add project files.

---
+++ LanguageApp/LanguageApp/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using LanguageApp.resx;
+using System;
+using System.Globalization;
+using System.Threading;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LanguageApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            /*CultureInfo ci = new CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+            AppResources.Culture = ci;
+
+            AppResources.Culture = LocalizationResourceManager.Current.CurrentCulture;*/
+            LocalizationResourceManager.Current.PropertyChanged += Current_PropertyChanged;
+            LocalizationResourceManager.Current.Init(AppResources.ResourceManager);
+
+
+            /*
+            CultureInfo info = new CultureInfo("fr");
+            Thread.CurrentThread.CurrentUICulture = info;
+            AppResources.Culture = info;*/
+
+            MainPage = new MainPage();
+        }
+        private void Current_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            AppResources.Culture = LocalizationResourceManager.Current.CurrentCulture;
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ LanguageApp/LanguageApp/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using LanguageApp.resx;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace LanguageApp
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public class LanguagePair
+        {
+            public Func<string> Name { get; set; }
+            public string NameString { get { return Name(); } }
+            public string Value { get; set; }
+        }
+        public List<LanguagePair> languageMapping { get; }
+        public LocalizedString CurrentLanguage { get; }
+        LanguagePair _selectedLanguage;
+        public LanguagePair SelectedLanguage
+        {
+            get { return _selectedLanguage; }
+            set
+            {
+                if (_selectedLanguage != value)
+                {
+                    _selectedLanguage = value;
+                    LocalizationResourceManager.Current.CurrentCulture = value.Value == null ? CultureInfo.CurrentCulture : new CultureInfo(value.Value);
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public MainViewModel()
+        {
+            languageMapping = new List<LanguagePair>();
+            languageMapping.Add(new LanguagePair { Name = () => AppResources.English, Value = "en" });
+            languageMapping.Add(new LanguagePair { Name = () => AppResources.French, Value = "fr" });
+
+            CurrentLanguage = new LocalizedString(() => GetCurrentLanguageName());
+            SelectedLanguage = languageMapping.SingleOrDefault(m => m.Value == LocalizationResourceManager.Current.CurrentCulture.TwoLetterISOLanguageName);
+        }
+        private string GetCurrentLanguageName()
+        {
+            string name = languageMapping.SingleOrDefault(m => m.Value == LocalizationResourceManager.Current.CurrentCulture.TwoLetterISOLanguageName).Name();
+            return name != null ? name : LocalizationResourceManager.Current.CurrentCulture.DisplayName;
+        }
+    }
+}
